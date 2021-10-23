3c2472a0434a3b5290766768aa4a209161964255
RuizhiZuo <93020757+RuizhiZuo@users.noreply.github.com>
2021-10-23 04:00:09 -0400

    To-do basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Component/Header';
+import List from './Component/List';
+import Footer from './Component/Footer'
+
+export default class App extends Component {
+
+  state = {
+    todos: [
+      { id: '001', name: 'chifan', done: true },
+      { id: '002', name: 'shuijiao', done: true },
+      { id: '003', name: 'coding', done: false },
+      { id: '004', name: 'coding', done: false },
+      { id: '005', name: '逛街', done: true },
+    ]
+  }
+  
+  addTodo=(todoObj)=>{
+    const {todos}=this.state
+    const newTodos=[todoObj,...todos]
+    this.setState({todos:newTodos})
+  }
+
+
+  render() {
+    const { todos } = this.state
+    return (
+      <div className='todo-container'>
+        <div className='todo-wrap'>
+          <Header addTodo={this.addTodo}/>
+          <List todos={todos} />
+          <Footer />
+        </div>
+
+      </div>
+    )
+  }
 }
-
-export default App;
